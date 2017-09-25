4f838b8b6bcb48159ff46c7b011c96373e7eedce
chainly <1258626769@qq.com>
2017-09-25 11:35:23 +0800

    Update __init__.py

--- speedlimit/__init__.py
+++ speedlimit/__init__.py
@@ -1,3 +1,23 @@
+#coding: utf8
+"""
+@Ctime 2017-09-22 19:20:07
+@Mtime 2017-09-22 19:20:11
+@Author COOl(1258626769@qq.com)
+@Ref: https://github.com/harlowja/speedlimit/blob/master/speedlimit/__init__.py
+    https://en.wikipedia.org/wiki/Token_bucket
+    http://lartc.org/lartc.html#AEN690
+    processbar
+The token bucket algorithm can be conceptually understood as follows:
+
+A token is added to the bucket every {\displaystyle 1/r} 1/r seconds.
+The bucket can hold at the most {\displaystyle b} b tokens. If a token arrives when the bucket is full, it is discarded.
+When a packet (network layer PDU) of n bytes arrives, n tokens are removed from the bucket, and the packet is sent to the network.
+If fewer than n tokens are available, no tokens are removed from the bucket, and the packet is considered to be non-conformant.
+
+@Next: make _bucket <= init_bucket
+    make min independent
+"""
+
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 # Copyright 2010 United States Government as represented by the
@@ -18,53 +38,86 @@
 
 import math
 import time
+import socket
+class TooSlowError(socket.error):
+    pass
 
+# http://www.cnblogs.com/memo-store/p/5658277.html
 from monotonic import monotonic as _now
 
 
 class SpeedLimit(object):
     """Speed/limiting iterator wrapper object.
-
     A wrapper object that uses the `token bucket`_ algorithm to limit the
     rate at which values comes out of an iterable. This can be used to limit
     the consumption speed of iteration of some other iterator (or iterable).
-
     .. _token bucket: http://en.wikipedia.org/wiki/Token_bucket
     """
     def __init__(self,
                  # How many items to yield from the provided
                  # wrapped iterator (per second).
-                 items_per_second,
+                 items_per_second=0,
                  # Used to simulate a thread with its own 'tic rate'. Making
                  # this smaller affects the accuracy of the 'tic' calculation,
                  # which affects the accuracy of consumption (and delays).
-                 refresh_rate_seconds=0.01,
-                 # How *full* the initial bucket is.
+                 refresh_rate_seconds=1,
+                 # length of the bucket
                  initial_bucket_size=1,
+                 # min or raise
+                 min_per_second=0,
+                 check_interval=5,
+                 too_slow_count=10, 
                  # Made a keyword argument, so one could replace this
                  # with a eventlet.sleep or other idling function...
                  sleep_func=time.sleep):
+        # unset items_per_second to not limit
+        if not items_per_second:
+            items_per_second = 10**10 # 10GB
+        assert min_per_second <= items_per_second, 'min > max!'
+        
         self._refresh_rate_seconds = refresh_rate_seconds
         self._bucket = (items_per_second *
                         refresh_rate_seconds * initial_bucket_size)
         self._items_per_tic = items_per_second * refresh_rate_seconds
         self._next_fill = _now() + refresh_rate_seconds
         self._sleep = sleep_func
+        
+        # min
+        # unset min_per_second to not raise
+        if not min_per_second:
+            check_interval = 10**10 # to long
+            
+        self._last_check = _now()
+        self._last_size = self._bucket
+        self._check_interval = check_interval
+        self._max_left_per_second = (items_per_second-min_per_second) * initial_bucket_size
+        self._slow_count = 0
+        self._too_slow_count = too_slow_count
 
     def _check_fill(self):
         # Fill the bucket based on elapsed time.
         #
         # This simulates a background thread...
-        now = _now()
+        now = _now() 
         if now > self._next_fill:
             d = now - self._next_fill
             tics = int(math.ceil(d / self._refresh_rate_seconds))
             self._bucket += tics * self._items_per_tic
             self._next_fill += tics * self._refresh_rate_seconds
+            
+        if now - self._last_check > self._check_interval:
+            _d = now - self._last_check
+            _max_left = self._last_size + self._max_left_per_second * _d
+            if self._bucket > _max_left:
+                self._slow_count += 1
+                print('*TooSlow %s*, %s > %s' % (self._slow_count, self._bucket ,_max_left))
+                if self._slow_count >= self._too_slow_count:
+                    raise TooSlowError('%s>%s' % (self._bucket, _max_left))
+            self._last_size = self._bucket  
+            self._last_check = now        
 
-    def speed_limit_iter(self, itr, chunk_size_cb=None):
+    def speed_limit_iter(self, itr, chunk_size_cb=len):
         """Return an iterator/generator which limits after each iteration.
-
         :param itr: an iterator to wrap
         :param chunk_size_cb: a function that can calculate the
                               size of each chunk (if none provided this
@@ -87,3 +140,16 @@ class SpeedLimit(object):
                 self._check_fill()
             self._bucket -= sz
             yield chunk
+            
+            
+if __name__ == '__main__':
+    import urllib2
+    url = 'http://sw.bos.baidu.com/sw-search-sp/software/d28b12c330f7b/android-studio-bundle_2.2.0.0.exe'
+    
+    fp = urllib2.urlopen(url)
+    police = SpeedLimit(min_per_second=10000000, check_interval=2,)
+    
+    for alpha in police.speed_limit_iter(fp):
+        print(police._bucket, _now(), police._last_size, police._slow_count,)
+        print(len(alpha))
+    
